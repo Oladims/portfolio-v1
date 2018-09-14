2dbfd708f2ad108b2e8d022197ff500014f75a1b
Oladims <demolaswift@outlook.com>
2018-09-14 17:21:18 +0100

    Added Project Files

--- app.js
+++ app.js
@@ -1,50 +1,78 @@
-animatecolor = () => setInterval(changeColor, 1000);
+animatecolor = () => setInterval(changeColor, 2000);
 // Selects a background color from a set of random colors at intervals
 changeColor = () => {
-  let bgcolors = ['#fff', '#f0532c', '#3db161', '#e8c0ff', '#bd9986'];
-  let bgcolor = bgcolors[Math.floor(Math.random() * bgcolors.length)];
-    document.body.style.background = bgcolor;
-}
-// animatecolor();
+  let rainbowText = document.getElementsByClassName('rainbow');
+  let bgcolors1 = ['#fd3', '#f0532c', '#3db161', '#e8c0ff'];
+  let bgcolors2 = ['#FF00FF', '#000080', '#008080', '##00FFFF'];
+  let bgcolors3 = ['#000000', '#808000', '#FF5733', '#bd9986'];
+  let bgcolor1 = bgcolors1[Math.floor(Math.random() * bgcolors1.length)];
+  let bgcolor2 = bgcolors2[Math.floor(Math.random() * bgcolors2.length)];
+  let bgcolor3 = bgcolors3[Math.floor(Math.random() * bgcolors3.length)];
+    // document.body.style.background = bgcolor;
 
-
-var theThing = document.querySelector("#thing");
-var container = document.getElementsByTagName("body");
- 
-container.addEventListener("click", getClickPosition, false);
- 
-function getClickPosition(e) {
-    var parentPosition = getPosition(e.currentTarget);
-    var xPosition = e.clientX - parentPosition.x - (theThing.clientWidth / 2);
-    var yPosition = e.clientY - parentPosition.y - (theThing.clientHeight / 2);
-     
-    theThing.style.left = xPosition + "px";
-    theThing.style.top = yPosition + "px";
+    rainbowText[0].style.color = bgcolor1;
+    rainbowText[1].style.color = bgcolor2;
+    rainbowText[2].style.color = bgcolor3;
+    rainbowText[3].style.color = bgcolor2;
+    rainbowText[4].style.color = bgcolor1;
+    rainbowText[5].style.color = bgcolor3;
+    rainbowText[6].style.color = bgcolor2;
+    rainbowText[7].style.color = bgcolor1;
+    rainbowText[8].style.color = bgcolor3;
+    rainbowText[9].style.color = bgcolor2;
 }
- 
-// Helper function to get an element's exact position
-function getPosition(el) {
-  var xPos = 0;
-  var yPos = 0;
- 
-  while (el) {
-    if (el.tagName == "BODY") {
-      // deal with browser quirks with body/window/document and page scroll
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
- 
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      // for all other non-BODY elements
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
- 
-    el = el.offsetParent;
+animatecolor();
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+// ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+  if (!isDrawing) return; // stop the fn from running when they are not moused down
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
   }
-  return {
-    x: xPos,
-    y: yPos
-  };
+  if (ctx.lineWidth >= 10 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+
 }
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
