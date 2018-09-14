6ce2681ae95845f604ad5a913bba1c5a8d65680d
Ibrahim Olufowora <ioolofowora@sidmach.com>
2018-09-14 10:56:03 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+animatecolor = () => setInterval(changeColor, 1000);
+// Selects a background color from a set of random colors at intervals
+changeColor = () => {
+  let bgcolors = ['#fff', '#f0532c', '#3db161', '#e8c0ff', '#bd9986'];
+  let bgcolor = bgcolors[Math.floor(Math.random() * bgcolors.length)];
+    document.body.style.background = bgcolor;
+}
+// animatecolor();
+
+
+var theThing = document.querySelector("#thing");
+var container = document.getElementsByTagName("body");
+ 
+container.addEventListener("click", getClickPosition, false);
+ 
+function getClickPosition(e) {
+    var parentPosition = getPosition(e.currentTarget);
+    var xPosition = e.clientX - parentPosition.x - (theThing.clientWidth / 2);
+    var yPosition = e.clientY - parentPosition.y - (theThing.clientHeight / 2);
+     
+    theThing.style.left = xPosition + "px";
+    theThing.style.top = yPosition + "px";
+}
+ 
+// Helper function to get an element's exact position
+function getPosition(el) {
+  var xPos = 0;
+  var yPos = 0;
+ 
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+ 
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+ 
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
+}
